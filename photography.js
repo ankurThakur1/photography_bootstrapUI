3cf3291dcc4941e0eff162d426d3d9e334b18d5a
ankurThakur1 <ankurdev.react+github@gmail.com>
2024-04-27 21:33:51 +0530

    added to github

---
+++ photography.js
@@ -0,0 +1,39 @@
+console.log("Photography website for interview");
+
+const imgCol = document.querySelectorAll(".img-col");
+const modalContainer = document.querySelector(".modal-container");
+
+imgCol.forEach((item) => {
+    item.addEventListener("click", function(e) {
+        // console.log(e);
+        console.log(e.target.src);
+        const modalImage = e.target.src;
+        showModal(modalImage);
+    });
+});
+
+
+const showModal = (imageSrc) => {
+    const modalImage = document.createElement("div");
+    modalImage.classList.add("modal-image");
+    modalImage.innerHTML = `
+        <img src="${imageSrc}" >
+        <button class="imgBtn" onclick=removeModal()><i class="fa-solid fa-xmark"></i></button>
+    `;
+    modalContainer.innerHTML = "";
+    modalContainer.appendChild(modalImage);
+    modalContainer.classList.add("show-modal");
+}
+
+const removeModal = () => {
+    modalContainer.classList.remove("show-modal");
+}
+
+
+   // const modalImage = document.createElement("img");
+    // console.log(modalImage);
+    // modalImage.src = imageSrc;
+    // modal.innerHTML = "";
+    // modal.appendChild(modalImage);
+    // // console.log(image);
+    // modal.classList.add("show-modal");
